3b1bf42c7474abe4e912a4f21ccf5b8be7d6c761
khanasjad <57167720+khanasjad@users.noreply.github.com>
2021-07-29 23:57:51 -0700

    Changed to java

--- Programs/Insertionsort.java
+++ Programs/Insertionsort.java
@@ -14,16 +14,19 @@ class Main {
 
 
 
-        for (int i =0; i<array.length;i++){
-            for(int j= 1; j<array.length;j++){
-                if(array[j-1]>array[j]) {
-                    int temp = array[j];
-                    array[j] =array[j-1];
-                    array[j-1] = temp;
-                }
+        for (int i =0; i<array.length;i++) {
+        int  temp = array[i];
+        int j = i-1;
+
+            while(j>=0 && array[j]>temp){
+                array[j+1] = array[j];
+                j--;
             }
+            array[j+1]= temp;
         }
         System.out.println(Arrays.toString(array));
 
         }
     }
+
+
